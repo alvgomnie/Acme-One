70e56d398c18369db9eb162d0d49797cd87f4050
alvgomnie <alvgomnie@alum.us.es>
2022-03-18 11:02:08 +0100

    Arreglo Patronage

--- src/main/java/acme/entities/Patronage.java
+++ src/main/java/acme/entities/Patronage.java
@@ -2,6 +2,8 @@ package acme.entities;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.validation.Valid;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
@@ -11,6 +13,8 @@ import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.URL;
 
 import acme.framework.entities.AbstractEntity;
+import acme.roles.Inventor;
+import acme.roles.Patron;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -43,15 +47,14 @@ public class Patronage extends AbstractEntity{
 	@URL
 	protected String infoURL;
 	
-//	@NotNull
-//	@Valid
-//	@ManyToOne
-//	protected Patron patron;
+	@NotNull
+	@Valid
+	@ManyToOne
+	protected Patron patron;
 	
-//	@NotNull
-//	@Valid
-//	@ManyToOne(optional=false)
-//	protected Inventor inventor;
-//	
+	@NotNull
+	@Valid
+	@ManyToOne(optional=false)
+	protected Inventor inventor;
 
 }

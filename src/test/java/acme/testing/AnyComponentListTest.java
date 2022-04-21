454a45533345a6c5a2111abc60d67789ad7d1cea
juacasalb <juacasalb@alum.us.es>
2022-04-21 18:49:25 +0200

    D3-03 & D3-04 - Tests Done

--- src/test/java/acme/testing/AnyComponentListTest.java
+++
@@ -1,6 +0,0 @@
-package acme.testing;
-
-
-public class AnyComponentListTest extends TestHarness{
-
-}
---
+++ src/test/java/any/item/AnyToolListTest.java
@@ -0,0 +1,55 @@
+package any.item;
+
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import acme.testing.TestHarness;
+
+public class AnyToolListTest extends TestHarness {
+
+	@ParameterizedTest
+	@CsvFileSource(resources = "/any/item/tool.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@Order(10)
+	public void anonymousListComponents(final int testIndex, final String name, final String type) {
+	
+		super.clickOnMenu("Items", "List of Tools");
+		super.checkListingExists();
+		super.sortListing(0, "asc");
+		
+		super.checkColumnHasValue(testIndex, 0, name);
+		super.checkColumnHasValue(testIndex, 1, type);
+		
+		super.clickOnListingRecord(testIndex);
+		
+		super.checkFormExists();
+		super.checkInputBoxHasValue("name", name);
+		super.checkInputBoxHasValue("type", type);
+	}
+	
+	
+	@ParameterizedTest
+	@CsvFileSource(resources = "/any/item/tool.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@Order(10)
+	public void administratorListComponents(final int testIndex, final String name, final String type) {
+
+		super.signIn("administrator", "administrator");
+	
+		super.clickOnMenu("Items", "List of Tools");
+		super.checkListingExists();
+		super.sortListing(0, "asc");
+		
+		super.checkColumnHasValue(testIndex, 0, name);
+		super.checkColumnHasValue(testIndex, 1, type);
+		
+		super.clickOnListingRecord(testIndex);
+		
+		super.checkFormExists();
+		super.checkInputBoxHasValue("name", name);
+		super.checkInputBoxHasValue("type", type);
+
+	
+		super.signOut();
+		
+	}
+}
